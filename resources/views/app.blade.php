7e4641ec3d0faaf4cdab9266266d3e5a56ed6ba7
ekox <ekox69@gmail.com>
2024-10-04 09:54:49 +0700

    perbaikan ref akun

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -149,7 +149,7 @@
 
             <!-- BEGIN : Footer-->
             <footer class="footer undefined undefined">
-                <p class="clearfix text-muted m-0"><span>Copyright &copy; 2020 &nbsp;</span><a href="https://themeforest.net/user/pixinvent/portfolio?ref=pixinvent" id="pixinventLink" target="_blank">PIXINVENT</a><span class="d-none d-sm-inline-block">, All rights reserved.</span></p>
+                <p class="clearfix text-muted m-0"><span>Copyright &copy; 2024 &nbsp;</span><a href="javascript:;" id="pixinventLink" target="_blank">The X Team</a><span class="d-none d-sm-inline-block">, All rights reserved.</span></p>
             </footer>
             <!-- End : Footer-->
             <!-- Scroll to top button -->
